479ac33673117c32bcac56ede7a3a575cc5752f4
Yoky0540 <naronthat@hotmail.co.th>
2023-08-13 11:55:11 +0700

    code commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ImageSlider from './components/ImageSlider'
+
+function App() {
+
+  return (
+    <div>
+      <h1>React Image Carousel</h1>
+      <ImageSlider/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ImageData.jsx
@@ -0,0 +1,28 @@
+const ImageData = [
+    {
+        title:"Meaw~~",
+        image:"https://cdn.pixabay.com/photo/2023/08/02/14/56/animal-8165466_1280.jpg"
+    },
+    {
+        title:"MeawPao",
+        image:"https://cdn.pixabay.com/photo/2016/10/01/20/42/cat-1708309_1280.jpg"
+    },
+    {
+        title:"Wanna Drink?",
+        image:"https://cdn.pixabay.com/photo/2017/08/07/13/47/cat-2603978_1280.jpg"
+    },
+    {
+        title:"Cat n Sun",
+        image:"https://cdn.pixabay.com/photo/2017/12/09/21/33/sunset-3008779_1280.jpg"
+    },
+    {
+        title:"Feeee..ZZZ",
+        image:"https://cdn.pixabay.com/photo/2019/05/08/21/21/cat-4189697_1280.jpg"
+    },
+    {
+        title:"Blue Eye White Catty",
+        image:"https://cdn.pixabay.com/photo/2017/02/15/12/12/cat-2068462_1280.jpg"
+    }
+]
+
+export default ImageData
